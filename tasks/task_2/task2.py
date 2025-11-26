641eecddf7bf91d2a4388f06209c2e2b32d3d656
Zhambyl Alikhan <alykhanzhambyl@icloud.com>
2025-11-26 17:43:36 +0500

    half of solution for task 2 with sorting key

--- tasks/task_2/task2.py
+++ tasks/task_2/task2.py
@@ -5,13 +5,11 @@ dir = Path("input")
 h = dir / "file_00.data"
 h = hashlib.sha3_256(h.read_bytes()).hexdigest()
 print(f'hash = {h}')
-d: int = 0
-# for i in h:
-#     i = int(i, base=16)
-#     d += i
-d = 0
-
-print(f'number version = {d}')
+k = 1
+for i in h:
+    k *= (int(i, 16) + 1)
+    
+print(f'sorting key = {k}')
 
 # for f in dir.iterdir():
 #     h = hashlib.sha3_256(f.read_bytes()).hexdigest()
@@ -19,6 +17,9 @@ print(f'number version = {d}')
 #     for i in h:
 #         i = int(i, base=16)
 #         i += 1
+# d = int(i, base=16)
+#     x = d + 1
+#     k = k * x 
     
 # for h in dir.iterdir():
 #     h = hashlib.sha3_256(h.read_bytes()).hexdigest()
