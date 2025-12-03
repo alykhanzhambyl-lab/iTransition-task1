e9c1f54c928d5304921348db0de43e8013ff33a6
Zhambyl Alikhan <alykhanzhambyl@icloud.com>
2025-12-03 15:32:09 +0500

    итоговый вариант. удаление лишних файлов

--- tasks/task_4/processing.py
+++
@@ -1,8 +0,0 @@
-# from revenue_m import ext_date, any_price_to_usd, paid_price_func, top5_days
-
-# def process_orders(orders_raw_df):
-#     orders_df = ext_date(orders_df)
-#     orders_df = any_price_to_usd(orders_df)
-#     orders_df = paid_price_func(orders_df)
-#     top5 = top5_days(orders_df) 
-#     return orders_df, top5
